e6417d3a3ad59cd0e26a3751ad48f076890c997d
JosipErvacic <128092036+Mili666999@users.noreply.github.com>
2023-12-14 20:58:22 +0100

    Unos podataka u tablicu smjer

---
+++ insert.sql
@@ -0,0 +1,14 @@
+use edunovawp3;
+
+select * from smjerovi;
+--skolska sintaksa
+insert into smjerovi (naziv,trajanje,cijena,vaucer)
+values ('Web programiranje',225,1859.45,1);
+
+--losa sintaksa
+insert into smjerovi values
+('Web dizajn',null,null,null);
+
+--minimalno dobra sintaksa
+
+insert into smjerovi(naziv) values ('šđčć ŽŠČ');

535c82e5dbe3076a517e4fa589125060fd330d12
Slaven Poznić <103325387+NoisyBoy123@users.noreply.github.com>
2024-01-23 19:34:08 +0100

    inserti

--- studio_ilustris_zavrsni.sql
+++ studio_ilustris_zavrsni.sql
@@ -1,24 +1,22 @@
 ﻿use master
 go
-drop database if exists frizerski_salon;
+drop database if exists studio_ilustris_zavrsni;
 go
-create database frizerski_salon;
+create database studio_ilustris_zavrsni;
 go
-use frizerski_salon;
+use studio_ilustris_zavrsni;
 
 create table djelatnici(
 sifra		int			not null	primary key identity (1,1),
 ime			varchar(50)	not null,
-prezime		varchar(50)	not null,
-oib			char(11)	not null,
-iban		varchar(50)
+prezime		varchar(50)	not null
 );
 
 create table korisnici(
 sifra		int			not null	primary key identity (1,1),
 ime			varchar(50)	not null,
 prezime		varchar(50)	not null,
-email		varchar(50),
+email		varchar(50) not null,
 djelatnik	int
 );
 
@@ -33,7 +31,7 @@ usluga		int
 create table usluge(
 sifra		int			not null	primary key identity (1,1),
 naziv		varchar(50)	not null,
-tip			varchar(50)
+tip			varchar(50) not null
 );
 
 
@@ -44,7 +42,36 @@ alter table posjeta add foreign key (usluga) references usluge;
 
 select * from djelatnici
 
-insert into djelatnici (ime,prezime,oib) values
+insert into djelatnici (ime,prezime) values
 --1
-('Dora','Dević',12345678911);
+('Dora','Dević'),
 --2
+('Katarina','Kovacic');
+
+
+select * from korisnici
+
+insert into korisnici(ime,prezime,email) values
+--1
+('Ana','Anic','aanic@gmail.com'),
+--2
+('Iva','Ivic','iivic@gmail.com'),
+--3
+('Matea','Matic','mmatic@gmail.com'),
+--4
+('Katarina','Katic','kkatic@gmail.com');
+
+
+select * from usluge
+
+insert into usluge(naziv,tip) values
+--1
+('Gel','Ugradnja'),
+--2
+('Gel','Nadopuna'),
+--3
+('Trajni_lak','Ugradnja'),
+--4
+('Trajni_lak','Nadopuna');
+
+
